463567b543de58f62a70688a943e3593f4f10862
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-09-27 10:02:13 -0400

    Create secondCharOfThirdString.js

---
+++ secondCharOfThirdString.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+let userArray = JSON.parse(prompt("please create an array: "));
+
+
+if(userarray[2 !== undefined && typeof userarray[2 === 'string']{
+
+let string = userarray[2];
+
+if (string.length >= 2{
+
+    console.log("the second character of the third item, in the array is ")
+    + string.substring(1,2) + "'.");
+{
+    else(
+        console.log("the string has ess thantwo characters");
+    
+}
+else{
+    console.log("error");
+}
+
