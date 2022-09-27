566a6ef0bb6adbad84fe50460d9e201626160d85
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-09-27 09:57:21 -0400

    Create isLongList.js

---
+++ isLongList.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+
+let userArray = JSON.parse(prompt("Enter an array: "));
+
+console.log(userarray);
+
+userarraylength = userarray.length
+console.log(userarraylength);
+
+boolean1 = false
+boolean2 = true
+
+if(userarraylength > 10){
+  console.log(false);
+}else if(userarraylength <= 10){
+    console.log(true);
+}
