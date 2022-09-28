23877b5ea38219f77ee7590112be1a1a4f4027c4
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-09-28 07:54:22 -0400

    Update getLastItemFrom.js

--- getLastItemFrom.js
+++ getLastItemFrom.js
@@ -4,4 +4,4 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let userArray = JSON.parse(prompt("Enter an array: "));
 
-console.log(userArray[userarray.length - 1])
+console.log(userArray[userArray.length - 1])
