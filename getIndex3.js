4af75ffb1fd394c5cbc313b92ab4a5b9b188c437
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-09 19:12:38 -0400

    Update getIndex3.js

--- getIndex3.js
+++ getIndex3.js
@@ -1,3 +1,9 @@
+//
+//Write a program getIndex3.js that prompts the user for an array then logs out the item at the 3rd index. 
+//If there are not four items (i.e., if there is no index 3), it logs out the value at the last index//
+
+
+
 const prompt = require('prompt-sync')({sigint: true});
 
 
@@ -8,5 +14,6 @@ console.log(userArray);
 if (userArray.length >= 3){
 console.log('the third item in the array is $(userarray [3]). ');
 }else {
-    console.log('the third item in the array is $(userarray[userArray.length - 1]}');
+    console.log('the third item in the array is $(userArray[userArray.length - 1]}');
 }
+
