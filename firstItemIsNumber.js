dfec2cbc1c421bb351424b73bedc33c08c9199ad
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-09 18:49:49 -0400

    Update firstItemIsNumber.js

--- firstItemIsNumber.js
+++ firstItemIsNumber.js
@@ -1,7 +1,11 @@
+/*firstItemIsNumber.js: prompts the user for an array then logs out a boolean indicating whether or not the first item in the array is a number
+*/
+
+
 const prompt = require('prompt-sync')({sigint: true});
 
 let Array = JSON.parse(prompt("Enter an array: "));
 
 
-console.log(typeof array[0] === "number");
+console.log(typeof Array[0] === "Number");
 
