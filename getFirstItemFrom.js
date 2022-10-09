aa52c20a7cb8c6b7828322f651b113bb72e157fc
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-09 18:57:04 -0400

    Update getFirstItemFrom.js

--- getFirstItemFrom.js
+++ getFirstItemFrom.js
@@ -1,5 +1,9 @@
+/*
+Write a program, getFirstItemFrom.js that prompts the user for an array, then logs out the first item in that array
+*/
+
 const prompt = require('prompt-sync')({sigint: true});
 
 let userArray = JSON.parse(prompt("Enter an array: "));
 
-console.log(userArray[0]);
+ console.log(userArray[0]);
