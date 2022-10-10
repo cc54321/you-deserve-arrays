8613d7d7017f13ead7f493730d3e1f78cc5d3d1f
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-09 20:13:18 -0400

    Update secondCharOfThirdString.js

--- secondCharOfThirdString.js
+++ secondCharOfThirdString.js
@@ -1,22 +1,31 @@
+/*
+secondCharOfThirdString.js: prompts the user for an array then logs the second character in the third item in the array. If the third item is not a string, log an error instead
+*/
+
+
+
+
 const prompt = require('prompt-sync')({sigint: true});
 
 let userArray = JSON.parse(prompt("please create an array: "));
 
 
-if(userArray[2 !== undefined && typeof userArray[2 === 'string']{
+if(userArray[2] !== undefined && typeof userArray[2] === 'string'){
 
-let string = userArray[2];
+let str = userArray[2];
 
-if (string.length >= 2{
+if (str.length >= 2){
 
-    console.log("the second character of the third item, in the array is ")
-    + string.substring(1,2) + "'.");
+    console.log("the second character of the third item, in the array is'")
+    + str.substring(1,2) + "'." };
+
+    }
+    else{
+        console.log("the string has Less than two characters")
+    } 
+
+
 {
-    else(
-        console.log("the string has Less than two characters");
-    
-}
-else{
-    console.log("error");
+    console.log("error")
 }
 
